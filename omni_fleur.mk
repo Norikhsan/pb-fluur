6968d57cd4d2c7b3a49b39e6c621f1026203866e
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-12-13 20:12:42 +0800

    Update omni_fleur.mk

--- omni_fleur.mk
+++ omni_fleur.mk
@@ -13,11 +13,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, device/xiaomi/fleur/device.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := omni_fleur
+PRODUCT_NAME := twrp_fleur
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := fleur
 PRODUCT_MANUFACTURER := xiaomi
